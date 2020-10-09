383ef7d652076f2ac5e974a0d0f7ad5041c7812e
Vadim Reger <vadoq8512@gmail.com>
2020-10-09 15:13:12 +1300

    Uploading the whole project
    All filles of Assignemnt 2

---
+++ tests/conftest.py
@@ -0,0 +1,47 @@
+import os
+import pytest
+
+from MovieWebApp import create_app
+from MovieWebApp.adapters import memory_repository
+from MovieWebApp.adapters.memory_repository import MemoryRepository
+
+# you must change in in order for the pytest to work
+TEST_DATA_PATH = os.path.join('A:',os.sep,'PythonProjects', 'Assignment 2', 'tests', 'data')
+
+
+@pytest.fixture
+def in_memory_repo():
+    repo = MemoryRepository()
+    memory_repository.populate(TEST_DATA_PATH, repo)
+    return repo
+
+
+
+@pytest.fixture
+def client():
+    my_app = create_app({
+        'TESTING': True,                                # Set to True during testing.
+        'TEST_DATA_PATH': TEST_DATA_PATH,               # Path for loading test data into the repository.
+        'WTF_CSRF_ENABLED': False                       # test_client will not send a CSRF token, so disable validation.
+    })
+
+    return my_app.test_client()
+
+
+class AuthenticationManager:
+    def __init__(self, client):
+        self._client = client
+
+    def login(self, username='thorke', password='cLQ^C#oFXloS'):
+        return self._client.post(
+            'authentication/login',
+            data={'username': username, 'password': password}
+        )
+
+    def logout(self):
+        return self._client.get('/auth/logout')
+
+
+@pytest.fixture
+def auth(client):
+    return AuthenticationManager(client)
